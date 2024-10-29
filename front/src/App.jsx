66ea0da60286ea201316c59ba11a3145d09de0fb
AhmadK <ahmad31100@live.fr>
2024-10-29 17:49:00 +0100

    start style with material ui

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,12 +3,23 @@ import Home from './pages/Home';
 import Solo from './pages/Solo';
 import Multiplayer from './pages/Multiplayer';
 import Header from './components/layout/Header';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  // Création du thème
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header />
+        <Header darkModeChecked={darkMode} setCheckedDarkMode={() => setDarkMode((prev) => !prev)} />
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -17,7 +28,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,62 +1,188 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Tooltip from '@mui/material/Tooltip';
+import Avatar from '@mui/material/Avatar';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 import logo from '../../assets/images/main_logo.webp';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-function Header() {
-    const location = useLocation();
+const pages = [
+    { name: 'Solo', path: '/solo' },
+    { name: 'Multijoueur', path: '/multiplayer' },
+];
+const settings = ['Mon compte', 'Déconnexion'];
+
+function Header({ darkModeChecked, setCheckedDarkMode }) {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
     return (
-        <header className="header">
-            <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-                <Link className="nav-link ms-2" to="/">
-                    <img src={logo} className="rounded-circle" height="80px" alt="Main Logo" />
-                </Link>
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse text-center" id="navbarColor01">
-                        <ul className="navbar-nav me-auto fs-5">
-                            <li className="nav-item">
-                                <Link
-                                    className={`nav-link ${location.pathname === '/solo' ? 'active' : ''}`}
-                                    to="/solo"
-                                >
-                                    Solo
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link
-                                    className={`nav-link ${location.pathname === '/multiplayer' ? 'active' : ''}`}
-                                    to="/multiplayer"
-                                >
-                                    Multijoueur
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <FontAwesomeIcon icon={faUser} size='3x' />
-                                </a>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <img
+                            src={logo}
+                            className="rounded-circle mt-2 mb-2"
+                            height="80px"
+                            alt="Main Logo"
+                        />
+                    </Typography>
 
-                                <small className="text-body-secondary text-capitalize">Bonjour,
-                                    NOM</small>
-                                <div className="dropdown-menu dropdown-menu-end text-center fs-5" aria-labelledby="dropdownMenuButton">
-                                    <a href="" className="dropdown-item">
-                                        Mon compte
-                                    </a>
-                                    <div className="dropdown-divider"></div>
-
-                                    <a className="dropdown-item text-danger fs-5" href="">Déconnexion</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                    <Typography component={Link} to={page.path} sx={{ textAlign: 'center', color: 'inherit', textDecoration: 'none' }}>
+                                        {page.name}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component={Link}
+                        to="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <img
+                            src={logo}
+                            className="rounded-circle mt-2 mb-2"
+                            height="80px"
+                            alt="Main Logo"
+                        />
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                component={Link}
+                                to={page.path}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 5,
+                                    mr: 3,
+                                    color: 'white',
+                                    display: 'block',
+                                    fontSize: '1.2rem',
+                                }}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                    <FormControlLabel label="Mode sombre" control={<Switch checked={darkModeChecked} onChange={setCheckedDarkMode}  defaultChecked />} />
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Mon profil">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="/static/images/avatar/2.jpg"
+                                    sx={{ width: 56, height: 56 }}
+                                />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '70px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
