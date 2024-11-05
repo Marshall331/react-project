941f8195ea08e53e586ff2ea9c7b5c50a20b767e
AhmadK <ahmad31100@live.fr>
2024-11-05 23:02:06 +0100

    Working on password reset

--- front/src/components/Auth/SignIn/ForgotPassword.jsx
+++ front/src/components/Auth/SignIn/ForgotPassword.jsx
@@ -7,18 +7,65 @@ import DialogContentText from '@mui/material/DialogContentText';
 import Typography from '@mui/material/Typography';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 function ForgotPassword({ open, handleClose }) {
+
+  const [loading, setLoading] = useState(false);
+
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+
+  const handleSubmit = async (email) => {
+    setLoading(true)
+
+    console.log(email)
+
+    // const access = {
+    //   email,
+    // }
+
+    try {
+      // const response = await loginToApp(access)
+
+      // if (response.status === 200) {
+        // setLoading(false);
+        // setConnectionFailed(false);
+        // setConnectionFailedMessage('');
+
+        // login(rememberMe)
+      // }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const validateInputs = () => {
+
+    const email = document.getElementById('emailReset').value;
+
+    let isValid = true;
+
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+      setEmailError(true);
+      setEmailErrorMessage('Merci de saisir une adresse e-mail valide.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    if (isValid) {
+      handleSubmit(email)
+    }
+  };
+
   return (
     <Dialog
       open={open}
       onClose={handleClose}
       PaperProps={{
-        component: 'form',
-        onSubmit: (event) => {
-          event.preventDefault();
-          handleClose();
-        },
         sx: { backgroundImage: 'none' },
       }}
     >
@@ -31,14 +78,14 @@ function ForgotPassword({ open, handleClose }) {
         sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
       >
         <DialogContentText>
-          Entrez l&apos;adresse e-mail de votre compte, et nous vous enverrons un lien pour
+          Entrez l&apos;adresse e-mail de votre compte et nous vous enverrons un lien pour
           réinitialiser votre mot de passe.
         </DialogContentText>
         <FormControl>
           <TextField
-            // error={emailError}
-            // helperText={emailErrorMessage}
-            id="email"
+            error={emailError}
+            helperText={emailErrorMessage}
+            id="emailReset"
             type="email"
             name="email"
             label="E-mail"
@@ -52,9 +99,16 @@ function ForgotPassword({ open, handleClose }) {
       </DialogContent>
       <DialogActions sx={{ pb: 3, px: 3 }}>
         <Button onClick={handleClose}>Annuler</Button>
-        <Button variant="contained" type="submit">
-          Continuer
-        </Button>
+        <LoadingButton
+            type='submit'
+            color="primary"
+            onClick={validateInputs}
+            loading={loading}
+            variant="contained"
+            size='medium'
+          >
+            Réinitialiser
+          </LoadingButton>
       </DialogActions>
     </Dialog>
   );
